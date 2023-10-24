509b62e4a466882f334f63d54d9d836a8cee7551
HuynhKhoa1601 <huynhnhatkhoa21@gmail.com>
2023-10-24 15:12:45 +0700

    add task 5 and robot to open Chrome automatically

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import time
 from scheduler import *
 from task2 import *
 from task3 import *
+from task5 import *
 
 scheduler = Scheduler()
 scheduler.SCH_Init()
 
 task2 = Task2()
 task3 = Task3()
+task5 = Task5()
 
 
 scheduler.SCH_Add_Task(task2.Task2_Run, 1000, 5000)
 scheduler.SCH_Add_Task(task3.Task3_Run, 1000, 5000)
-
+scheduler.SCH_Add_Task(task5.Task5_Run, 1000, 5000)
 
 while True:
     scheduler.SCH_Update()
