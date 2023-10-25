ff7e61d5ccbd0403721c3854c595d7296e3e07ab
HuynhKhoa1601 <huynhnhatkhoa21@gmail.com>
2023-10-25 12:01:47 +0700

    make the robot fill in the form

--- task5.py
+++ task5.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+import time
 
 chrome_options = webdriver.ChromeOptions()
 
@@ -12,5 +15,30 @@ class Task5:
         return
 
     def Task5_Run(self):
-        browser.get('http://selenium.dev/')
+        browser.get('https://robotsparebinindustries.com/')
+        time.sleep(4)
+        username_input = browser.find_element(By.ID, "username")
+        password_input = browser.find_element(By.ID, "password")
+
+        username_input.send_keys("maria")
+        password_input.send_keys("thoushallnotpass")
+
+        submit_button = browser.find_element(By.XPATH,'//*[@id="root"]/div/div/div/div[1]/form/button')
+        submit_button.click()
+
+        time.sleep(3)
+
+        firstname_input = browser.find_element(By.ID, "firstname")
+        lastname_input = browser.find_element(By.ID, "lastname")
+        sale_result_input = browser.find_element(By.ID,"salesresult")
+        firstname_input.send_keys("Lock")
+        lastname_input.send_keys("Huynh")
+        sale_result_input.send_keys("123")
+
+        sales_target_element = browser.find_element(By.ID, 'salestarget')
+        sales_target_select = Select(sales_target_element)
+        sales_target_select.select_by_value('10000')
+        submit_button = browser.find_element(By.XPATH,'//*[@id="sales-form"]/button')
+        submit_button.click()
+
         print("Task 5 is activated!!!!")
