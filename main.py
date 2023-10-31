e58c946a40a7e8e298919de8ddddb4fa8a526186
Hung111203 <10422031@student.vgu.edu.vn>
2023-10-31 09:59:42 +0700

    update speech-to-text

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import time
 
 from scheduler import *
+from task1 import *
 from task2 import *
 from task3 import *
 
 scheduler = Scheduler()
 scheduler.SCH_Init()
 
+task1= Task1()
 task2 = Task2()
 task3 = Task3()
 
+scheduler.SCH_Add_Task(task1.Task1_Run, 1000, 5000)
 scheduler.SCH_Add_Task(task2.Task2_Run, 1000, 5000)
 scheduler.SCH_Add_Task(task3.Task3_Run, 1000, 5000)
 
--- task1.py
+++ task1.py
@@ -1,53 +1,33 @@
-from keras.models import load_model  # TensorFlow is required for Keras to work
-import cv2  # Install opencv-python
 import numpy as np
-import tensorflow.compat.v2 as tf
+import speech_recognition as sr
+
 
 class Task1:
     model = None
     class_names = None
+    recognizer = None
 
-    def __init__(self, index, url):
+
+    def __init__(self):
         print("Init task 1")
-        np.set_printoptions(suppress=True)
+        self.recognizer = sr.Recognizer()
 
-        # Load the model
-        self.model = load_model("keras_Model.h5", compile=False)
-
-        # Load the labels
-        self.class_names = open("labels.txt", "r").readlines()
-
-        # CAMERA can be 0 or 1 based on default camera of your computer
-        self.camera = cv2.VideoCapture(index)
-        self.cameraID =  index
-        return
 
     def Task1_Run(self):
         print("Task 1 is activated!!!!")
-        # Grab the webcamera's image.
-        ret, image = self.camera.read()
-        cv2.imwrite("task"+ str(self.cameraID) + ".png", image)
+        audio_file = "C:/Users/LENOVO/VGU_IntroToCS_Project.git/trunk/audio-2.wav"
+        print("Using audio file:", audio_file)
+        result = self.audio_to_text(audio_file)
+        print("Transcribed text:", result)
 
-        # Resize the raw image into (224-height,224-width) pixels
-        image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+    def audio_to_text(self, audio_file):
+        with sr.AudioFile(audio_file) as source:
+            audio = self.recognizer.record(source, duration=10)
 
-        # Show the image in a window
-        #cv2.imshow("Webcam Image", image)
-
-
-        # Make the image a numpy array and reshape it to the models input shape.
-        image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-
-        # Normalize the image array
-        image = (image / 127.5) - 1
-
-        # Predicts the model
-        prediction = self.model.predict(image)
-        index = np.argmax(prediction)
-        class_name = self.class_names[index]
-        confidence_score = prediction[0][index]
-
-        # Print prediction and confidence score
-        print("Class:", class_name[2:], end="")
-        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-
+        try:
+            text = self.recognizer.recognize_google(audio)
+            return text
+        except sr.UnknownValueError:
+            return "Google Speech Recognition could not understand the audio"
+        except sr.RequestError as e:
+            return f"Could not request results from Google Speech Recognition service; {e}"        
