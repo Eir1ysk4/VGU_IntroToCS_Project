a2dc11553acd4d4bec66b2ee71e0ec36ca8b670b
Hung111203 <10422031@student.vgu.edu.vn>
2023-11-07 22:52:29 +0700

    change in speech to text

--- task1.py
+++ task1.py
@@ -1,33 +1,28 @@
-import numpy as np
 import speech_recognition as sr
 
-
 class Task1:
-    model = None
-    class_names = None
     recognizer = None
-
 
     def __init__(self):
         print("Init task 1")
         self.recognizer = sr.Recognizer()
 
-
     def Task1_Run(self):
         print("Task 1 is activated!!!!")
-        audio_file = "C:/Users/LENOVO/VGU_IntroToCS_Project.git/trunk/audio-2.wav"
-        print("Using audio file:", audio_file)
-        result = self.audio_to_text(audio_file)
-        print("Transcribed text:", result)
+        with sr.Microphone() as source:
+            print("Listening for 10 seconds. Speak now")
+            audio = self.recognizer.listen(source, timeout=10)
+            try:
+                text = self.recognizer.recognize_google(audio)
+                print("You said:", text)
+                with open("file.txt", "w") as file:
+                    file.write(text)
+                    print("Recognized text saved to file.txt")
+            except sr.UnknownValueError:
+                print("Sorry, I couldn't understand what you said.")
+            except sr.RequestError as e:
+                print(f"Sorry, an error occurred: {e}")
 
-    def audio_to_text(self, audio_file):
-        with sr.AudioFile(audio_file) as source:
-            audio = self.recognizer.record(source, duration=10)
+task = Task1()
 
-        try:
-            text = self.recognizer.recognize_google(audio)
-            return text
-        except sr.UnknownValueError:
-            return "Google Speech Recognition could not understand the audio"
-        except sr.RequestError as e:
-            return f"Could not request results from Google Speech Recognition service; {e}"        
+task.Task1_Run()
