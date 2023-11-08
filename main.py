6695fa63f6967d95d4dfa088308e2957c7a49e72
HuynhKhoa1601 <huynhnhatkhoa21@gmail.com>
2023-11-08 10:57:32 +0700

    create function to search and play youtube

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import time
-
 from scheduler import *
 from task2 import *
 from task3 import *
--- task5.py
+++ task5.py
@@ -4,6 +4,8 @@ import openpyxl
 from xhtml2pdf import pisa
 from selenium.webdriver.support.select import Select
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 chrome_options = webdriver.ChromeOptions()
 
@@ -21,50 +23,71 @@ class Task5:
         return
 
     def Task5_Run(self):
-        browser.get('https://robotsparebinindustries.com/')
-        time.sleep(4)
-        username_input = browser.find_element(By.ID, "username")
-        password_input = browser.find_element(By.ID, "password")
+        self.play_Youtube_music_by_search("Khong ten")
+        # username_input = browser.find_element(By.ID, "username")
+        # password_input = browser.find_element(By.ID, "password")
+        #
+        # username_input.send_keys("maria")
+        # password_input.send_keys("thoushallnotpass")
+        # #
+        # submit_button = browser.find_element(By.XPATH,'//*[@id="root"]/div/div/div/div[1]/form/button')
+        # submit_button.click()
+        #
+        # time.sleep(3)
+        #
+        # browser.execute_script("window.location.href = 'https://robotsparebinindustries.com/SalesData.xlsx';")
+        #
+        # time.sleep(3)
+        #
+        # workbook = openpyxl.load_workbook("D:\\VGU\\Intro to CS\\project\\SalesData.xlsx")
+        #
+        # worksheet = workbook.active  # This selects the first sheet by default
+        #
+        # # Example: Read data from a specific cell
+        # # cell_value = sheet['A1'].value
+        #
+        # # Close the Excel file when you're done
+        # workbook.close()
+        #
+        # for row in worksheet.iter_rows(min_row=2, values_only=True):
+        #     # Assuming the first column (column A) contains the data you want
+        #     if (row[0]):
+        #         self.fill_and_submit_sales_form(row)
+        #
+        # time.sleep(3)
+        #
+        # browser.save_screenshot('C:\\Users\\ASUS\\Pictures\\Screenshots\\screenshot.png')
+        #
+        # time.sleep(3)
+        #
+        # self.export_as_pdf()
+        #
+        # time.sleep(3)
+        #
+        # self.log_out()
+    def search_page_on_browser(self, link):
+        browser.get(link)
+        time.sleep(5)
+    def play_Youtube_music_by_search(self, search_query):
+        self.search_page_on_browser("https://www.youtube.com/")
+        search_bar = browser.find_element(By.NAME, "search_query")
+        print(search_bar)
 
-        username_input.send_keys("maria")
-        password_input.send_keys("thoushallnotpass")
+        search_bar.send_keys(search_query)
 
-        submit_button = browser.find_element(By.XPATH,'//*[@id="root"]/div/div/div/div[1]/form/button')
-        submit_button.click()
+        search_bar_button = browser.find_element(By.ID, "search-icon-legacy")
+        search_bar_button.click()
 
-        time.sleep(3)
+        time.sleep(5)
 
-        browser.execute_script("window.location.href = 'https://robotsparebinindustries.com/SalesData.xlsx';")
-
-        time.sleep(3)
-
-        workbook = openpyxl.load_workbook("D:\\VGU\\Intro to CS\\project\\SalesData.xlsx")
-
-        worksheet = workbook.active  # This selects the first sheet by default
-
-        # Example: Read data from a specific cell
-        # cell_value = sheet['A1'].value
-
-        # Close the Excel file when you're done
-        workbook.close()
-
-        for row in worksheet.iter_rows(min_row=2, values_only=True):
-            # Assuming the first column (column A) contains the data you want
-            if (row[0]):
-                self.fill_and_submit_sales_form(row)
-
-        time.sleep(3)
-
-        browser.save_screenshot('C:\\Users\\ASUS\\Pictures\\Screenshots\\screenshot.png')
-
-        time.sleep(3)
-
-        self.export_as_pdf()
-
-        time.sleep(3)
-
-        self.log_out()
-
+        element = browser.find_element(By.XPATH, '//*[@id="contents"]/ytd-video-renderer[1]')
+        print(element)
+        element.click()
+        time.sleep(2)
+        browser.refresh()
+        time.sleep(2)
+        browser.refresh()
+        time.sleep(100)
 
     def fill_and_submit_sales_form(self, sales_rep):
 
