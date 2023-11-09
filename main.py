a9aed32b5af20df3d2294aaa6139eebe956f1a95
HuynhKhoa1601 <huynhnhatkhoa21@gmail.com>
2023-11-09 23:10:05 +0700

    remove pycache

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import time
 from scheduler import *
 from task2 import *
 from task4 import *
+from task6 import Task6
 
 scheduler = Scheduler()
 scheduler.SCH_Init()
 
-task2 = Task2()
-task3 = Task3()
+task4 = Task4()
+task6 = Task6()
 
-scheduler.SCH_Add_Task(task2.Task2_Run, 1000, 5000)
-scheduler.SCH_Add_Task(task3.Task3_Run, 1000, 5000)
+scheduler.SCH_Add_Task(task4.Task4_Run(), 1000, 5000)
+scheduler.SCH_Add_Task(task6.Task6_Run(task4), 1000, 5000)
+
 
 
 while True:
--- task4.py
+++ task4.py
@@ -5,32 +5,35 @@ import datetime as dt
 import requests
 BASE_URL = os.getenv("BASE_URL")
 API_KEY =  os.getenv('API_KEY')
-CITY = "Thanh pho Ho Chi Minh"
-class Task3:
+CITY = "London"
+class Task4:
+    collect_data = None
+
     def __init__(self):
-        print("Init task 3")
+        print("Init task 4")
     
         url =  BASE_URL + "appid=" + API_KEY+ "&q= " + CITY
         
         response = requests.get(url).json()
 
-        def kelvin_to_celsius_fahrenheit(kelvin):
-            celsius = kelvin - 273.15
-            fahrenheit = celsius*(9/5)+32
-            return celsius, fahrenheit
         
         self.temp_kelvin = response['main']['temp']
-        self.temp_celsius, self.temp_fahrenheit = kelvin_to_celsius_fahrenheit(self.temp_kelvin)
+        self.temp_celsius, self.temp_fahrenheit = self.kelvin_to_celsius_fahrenheit(self.temp_kelvin)
         feels_like_kelvin = response['main']['feels_like']
-        self.feels_like_celsius, self.feels_like_fahrenheit = kelvin_to_celsius_fahrenheit(feels_like_kelvin)
+        self.feels_like_celsius, self.feels_like_fahrenheit = self.kelvin_to_celsius_fahrenheit(feels_like_kelvin)
         self.wind_speed = response['wind']['speed']
         self.humidity = response ['main']['humidity']
         self.description = response['weather'][0]['description']
         self.sunrise_time =dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
         self.sunset_time =dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+        self.lon = response['coord']['lon']
+        self.lat = response['coord']['lat']
         return
-
-    def Task3_Run(self):
+    def kelvin_to_celsius_fahrenheit(self, kelvin):
+        celsius = kelvin - 273.15
+        fahrenheit = celsius * (9 / 5) + 32
+        return celsius, fahrenheit
+    def Task4_Run(self):
         print("Task 3 is activated!!!!")
         print(f"Temperature in {CITY}: {self.temp_celsius: .2f}°C or {self.temp_fahrenheit:.2f}°F")
         print(f"Temperature in {CITY} feels like: {self.feels_like_celsius: .2f}°C or {self.feels_like_fahrenheit:.2f}°F")
